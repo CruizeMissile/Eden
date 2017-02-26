c5511e0fdb5132a9878ffa84d23d83dd1b81ac24
CruizeMissile <james.simpson9339@gmail.com>
2017-02-26 15:29:10 -0500

    Generate make files in linux

--- generate-projects.py
+++ generate-projects.py
@@ -9,38 +9,35 @@ parser = argparse.ArgumentParser()
 
 
 def add_arguments():
-	pass
+    pass
 
 @contextmanager
 def pushd(new_dir):
-	prev_dir = os.getcwd()
-	os.chdir(new_dir)
-	yield
-	os.chdir(prev_dir)
+    prev_dir = os.getcwd()
+    os.chdir(new_dir)
+    yield
+    os.chdir(prev_dir)
 
 
 def main():
-	# parsing command line arguments
-	add_arguments()
-	args = parser.parse_args()
+# parsing command line arguments
+    add_arguments()
+    args = parser.parse_args()
 
-	current_dir = os.path.dirname(os.path.realpath(__file__))
+    current_dir = os.path.dirname(os.path.realpath(__file__))
 
-	if 'win32' == sys.platform:
-		command = ['Genie/windows/genie.exe', 'vs2015']
-	elif 'darwin' == sys.platform:
-		command = ['Genie/darwin/genie', 'xcode4']
-	else:
-		command = ['Genie/linux/genie', 'make']
+    if 'win32' == sys.platform:
+        command = ['./Genie/windows/genie.exe', 'vs2015']
+    elif 'darwin' == sys.platform:
+        command = ['./Genie/darwin/genie', 'xcode4']
+    else:
+        command = ['./Genie/linux/genie', 'gmake']
 
-	path = os.path.join(current_dir, "Scripts", "Project")
-	print (path)
-	with pushd(path):
-		print (os.getcwd())
-		subprocess.call(command)
-
-	return 0
+    path = os.path.join(current_dir, "Scripts", "Project")
+    with pushd(path):
+        subprocess.call(subprocess.list2cmdline(command), shell=True)
+    return 0
 
 
 if __name__ == '__main__':
-	sys.exit(main())
+    sys.exit(main())
