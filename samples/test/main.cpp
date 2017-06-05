9b9d388c1b4040c94a96a6e3ea25524c4330b3ec
cruizemissile <james.simpson9339@gmail.com>
2017-06-05 00:29:19 -0400

    Add test cases for events in the sample

--- samples/test/main.cpp
+++ samples/test/main.cpp
@@ -18,6 +18,17 @@ struct Something
 	Listener<evn::KeyUp> on_key_up;
 	Listener<evn::WindowEntered> on_window_enter;
 	Listener<evn::WindowExit> on_window_exit;
+
+	Something()
+	{
+		on_mouse_move.callback = [](auto& e) { cout << e.x << " : " << e.y << endl; };
+		on_mouse_down.callback = [](auto& e) { cout << static_cast<u32>(e.button) << endl; };
+		on_mouse_up.callback = [](auto& e) { cout << static_cast<u32>(e.button) << endl; };
+		on_key_down.callback = [](auto& e) { if (e.key == Key::Escape) App.running = false; cout << static_cast<u32>(e.key) << endl; };
+		on_key_up.callback = [](auto& e) { cout << static_cast<u32>(e.key) << endl; };
+		on_window_enter.callback = [](auto& e) { cout << "Entered" << endl; };
+		on_window_exit.callback = [](auto& e) { cout << "Exit" << endl; };
+	}
 };
 
 int engine()
@@ -25,14 +36,6 @@ int engine()
 	App.createWindow();
 
 	Something something;
-	something.on_mouse_move.callback = [](auto& e) { cout << e.x << " : " << e.y << endl; };
-	something.on_mouse_down.callback = [](auto& e) { cout << static_cast<u32>(e.button) << endl; };
-	something.on_mouse_up.callback = [](auto& e) { cout << static_cast<u32>(e.button) << endl; };
-	something.on_key_down.callback = [](auto& e) { cout << static_cast<u32>(e.key) << endl; };
-	something.on_key_up.callback = [](auto& e) { cout << static_cast<u32>(e.key) << endl; };
-	something.on_key_up.callback = [](auto& e) { cout << static_cast<u32>(e.key) << endl; };
-	something.on_window_enter.callback = [](auto& e) { cout << "Entered" << endl; };
-	something.on_window_exit.callback = [](auto& e) { cout << "Exit" << endl; };
 
 	while (App.running)
 	{
