495c2272b4a9a37c22cf11fa6bd6a70a93602430
James Simpson <james.simpson9339@gmail.com>
2018-03-30 09:55:50 -0400

    remove all files to have a clean slate

--- _unused/Math/Angle.cpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "Precompiled.h"
-#include "Math/Angle.h"
-
-namespace Math
-{
-    const f32 Degree::pi = 3.14159265358979323846f;
-    const f32 Radian::pi = 3.14159265358979323846f;
-}
--- tests/UnitTest.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-#include "catch.hpp"
-#include "Core/Types.h"
